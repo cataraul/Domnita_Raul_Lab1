a5c0946471f31ce1a755349196974dcc6e6e4447
cataraul <raul.domnita@yahoo.com>
2023-10-20 13:36:35 +0300

    Added new Controller and methods

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Domnita_Raul_Lab1.Controllers
+{
+    public class HelloController : Controller
+    {
+        public string Index()
+        {
+            return "Welcome!";
+        }
+
+        public string HelloThere()
+        {
+            return "Hello there, dear colleague!";
+        }
+
+        public string HowOld(string name, int age)
+        {
+            return $"So, {name}, you are {age} years old. Nice.";
+        }
+    }
+}
